a74b05d7bb423b1fa6298b3541ae2d46c2364551
Zhao, Yong <zonplm@gmail.com>
2018-10-31 09:00:48 -0400

    Add LICENSE file

--- DefSpy/SpyDefinitionPackage.cs
+++ DefSpy/SpyDefinitionPackage.cs
@@ -10,11 +10,13 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.Win32;
+using Task = System.Threading.Tasks.Task;
 
 namespace DefSpy
 {
@@ -40,7 +42,7 @@ namespace DefSpy
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(SpyDefinitionPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class SpyDefinitionPackage : Package
+    public sealed class SpyDefinitionPackage : AsyncPackage
     {
         /// <summary>
         /// SpyDefinitionCommandPackage GUID string.
@@ -60,16 +62,14 @@ namespace DefSpy
 
         #region Package Members
 
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        protected override void Initialize()
+        protected override Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             SetILPathCommand.Initialize(this);
             ShowDefinitionInILSpyCommand.Initialize(this);
-            base.Initialize();
+            return base.InitializeAsync(cancellationToken, progress);
         }
+
+
 
         #endregion
     }
